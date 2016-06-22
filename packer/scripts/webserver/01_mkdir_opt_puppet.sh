a644d8a99a1ba313d9a9dc06b0e8e33dca2ee509
XIN CHEN <xchen@opq.com.au>
2016-06-22 23:19:34 +1000

    Added application folder permission fix.

--- packer/scripts/webserver/01_mkdir_opt_puppet.sh
+++ packer/scripts/webserver/01_mkdir_opt_puppet.sh
@@ -3,3 +3,7 @@
 sudo -E sh -c "rm -rf /opt/puppet"
 sudo -E sh -c "mkdir -p /opt/puppet"
 sudo -E sh -c "chown -R ubuntu:ubuntu /opt/puppet"
+
+sudo -E sh -c "rm -rf /opt/application"
+sudo -E sh -c "mkdir -p /opt/application"
+sudo -E sh -c "chown -R ubuntu:ubuntu /opt/application"
